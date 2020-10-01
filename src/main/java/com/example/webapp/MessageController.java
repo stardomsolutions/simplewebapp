79eb82bf67ca220ac8cf826f5c80f5c23ee29f43
Stardom Solutions <stardomsolutionstraining@gmail.com>
2020-10-01 10:17:01 +0530

    test

--- src/main/java/com/example/webapp/MessageController.java
+++ src/main/java/com/example/webapp/MessageController.java
@@ -24,7 +24,7 @@ public class MessageController {
 
     @ResponseBody
     @RequestMapping(value = "/bye", method = RequestMethod.GET)
-    public String sayBye() {
+    public String sayHello() {
         return "bye";
     }
     
